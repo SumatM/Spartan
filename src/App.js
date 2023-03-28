f968315d00e26cbbe1bc536ee97f9bfccc80333a
SumatM <sumatmallick@gmail.com>
2023-03-28 13:38:06 +0530

    src folder changed

--- src/App.js
+++ src/App.js
@@ -1,23 +1,12 @@
-import logo from './logo.svg';
 import './App.css';
+import img from './Logo/logo-no-background.svg'
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    
+        <img src={img} style={{width:"15%",margin:'25px'}} alt="logo" />
+        
     </div>
   );
 }
