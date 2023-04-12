ba6a02dc2c26f2558f13982c629da0413c40c1cf
SumatM <sumatmallick@gmail.com>
2023-04-12 21:47:03 +0530

    loading skeleton responsivness done

---
+++ src/LoadingCard.jsx
@@ -0,0 +1,28 @@
+import { Skeleton,Box} from "@chakra-ui/react"
+
+ function LoadingCard(){
+    console.log('called');
+    
+    return(
+    <Box mt='50px'>
+        <Box >
+            <Skeleton m='auto' height='200px' width={{base:'65%',sm:"75%",md:'80%'}} />
+        </Box>
+        <br/>
+        <Box >
+            <Skeleton  height='20px' m='auto'  w={{base:'65%',sm:"75%",md:'80%'}}/>
+        </Box>
+        <br/>
+        <Box>
+            <Skeleton height='20px' m='auto'  w={{base:'65%',sm:"75%",md:'80%'}}/>
+        </Box>
+        <br/>
+        <Box>
+            <Skeleton height='20px' m='auto'  w={{base:'65%',sm:"75%",md:'80%'}}/>
+        </Box>
+        </Box>)
+
+        
+}
+
+export default LoadingCard;
